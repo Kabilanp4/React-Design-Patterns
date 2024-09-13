79ed002bf6108d72f23a403c744a861673d06148
Kabilan P <104247633+Kabilanp4@users.noreply.github.com>
2024-09-13 12:48:16 +0530

    SplitScreen Pattern

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
+import { SplitScreen } from "./SplitScreen";
 
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: "green" }}>{name}</h1>;
+};
+const RightHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: "yellow" }}>{name}</h1>;
+};
 function App() {
   return (
-    <div className="App">
-      <h1>This is so Cool!</h1>
-    </div>
+    <SplitScreen leftWeight={1} rightWeight={1}>
+      <LeftHandComponent name="React" />
+      <RightHandComponent name="Angular" />
+    </SplitScreen>
   );
 }
 
